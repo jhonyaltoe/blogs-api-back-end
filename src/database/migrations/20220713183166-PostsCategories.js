f08d188ef507a376f8ed54a69a09d65c8c6b3320
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-07-13 17:03:50 -0300

    feet:(migrations) cria arquivos no diretório migrations
    Cria arquivos que criam as colunas de todas as tabelas

---
+++ src/database/migrations/20220713183166-PostsCategories.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
