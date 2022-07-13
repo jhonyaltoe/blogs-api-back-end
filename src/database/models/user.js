ddb944b1b9a77a1e0a00650586d508bd75d39777
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-07-13 17:42:16 -0300

    feat:(user.js) cria o arquido user.js dentro do diretório models configurado corretamente

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+    tableName: 'Users',
+  });
+
+  return User;
+};
