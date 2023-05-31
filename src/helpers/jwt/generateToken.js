e2f6fdceb4ecdeb933a486269e42cd72e8da2963
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-05-31 11:31:11 -0300

    feat: add a variable apply default secretjwt

--- src/helpers/jwt/generateToken.js
+++ src/helpers/jwt/generateToken.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+const secret = process.env.JWT_SECRET || 'secretJWT'
+
 module.exports = (payload) => {
   const jwtConfig = {
     expiresIn: '7d',
     algorithm: 'HS256',
   };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, jwtConfig);
+  const token = jwt.sign(payload, secret, jwtConfig);
   return token;
 };
