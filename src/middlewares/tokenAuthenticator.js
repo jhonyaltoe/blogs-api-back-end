f7ba5200fe59192f6f3c8f5d4fe96147294a3754
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-06-04 09:35:01 -0300

    feat: add variable to secret jwt

--- src/middlewares/tokenAuthenticator.js
+++ src/middlewares/tokenAuthenticator.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 const { CustomError } = require('../helpers');
 
+const secret = process.env.JWT_SECRET || 'secretJWT'
+
 module.exports = (req, _res, next) => {
   if (!req.headers.authorization) throw new CustomError('Token not found', 401);
 
   try {
-    const data = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    const data = jwt.verify(req.headers.authorization, secret);
     req.body.user = data;
     next();
   } catch (error) {
